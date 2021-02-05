37a9439aa62d4679d8852f1b0d8ac7637f262243
Shovon13 <snakebite107@gmail.com>
2021-02-05 18:07:57 +0600

    login area done

---
+++ script.js
@@ -0,0 +1,5 @@
+var loginButton = document.getElementById('login-btn')
+loginButton.addEventListener('click', function(){
+    var loginArea = document.getElementById('login-area');
+    loginArea.style.display = 'none';
+})
