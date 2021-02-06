a6d2f32620e45ddb1904efb1deb42d49c94eefba
Shovon13 <snakebite107@gmail.com>
2021-02-06 23:52:55 +0600

    added more code

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-var loginButton = document.getElementById('login-btn')
+//Login button event handler
+const loginButton = document.getElementById('login-btn')
 loginButton.addEventListener('click', function(){
-    var loginArea = document.getElementById('login-area');
+    const loginArea = document.getElementById('login-area');
     loginArea.style.display = 'none';
+    const transactionArea = document.getElementById('transaction-area');
+    transactionArea.style.display = 'block';
+})
+//deposit button event handler
+const depositBtn = document.getElementById('add-deposit');
+depositBtn.addEventListener('click', function () {
+    const depositInput = document.getElementById('deposit-input').value;
+    const depositNumber = parseFloat(depositInput);
+
+    const currentDeposit = document.getElementById('current-deposit').innerText;
+    const currentDepositNumber = parseFloat(currentDeposit);
+    const totalDeposit = depositNumber + currentDepositNumber;
+
+    document.getElementById('current-deposit').innerText = totalDeposit;
+    document.getElementById('deposit-input').value = '';
 })
