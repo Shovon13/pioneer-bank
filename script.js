92918a7f9313459647f16240f7d55afa3e5e87b4
Shovon13 <snakebite107@gmail.com>
2021-02-07 22:56:10 +0600

    bank project done

--- script.js
+++ script.js
@@ -9,13 +9,40 @@ loginButton.addEventListener('click', function(){
 //deposit button event handler
 const depositBtn = document.getElementById('add-deposit');
 depositBtn.addEventListener('click', function () {
-    const depositInput = document.getElementById('deposit-input').value;
-    const depositNumber = parseFloat(depositInput);
+    // const depositInput = document.getElementById('deposit-input').value;
+    // const depositNumber = parseFloat(depositInput);
+    const depositNumber = getInputNumber('deposit-input')
 
-    const currentDeposit = document.getElementById('current-deposit').innerText;
-    const currentDepositNumber = parseFloat(currentDeposit);
-    const totalDeposit = depositNumber + currentDepositNumber;
+    // const currentDeposit = document.getElementById('current-deposit').innerText;
+    // const currentDepositNumber = parseFloat(currentDeposit);
+    // const totalDeposit = depositNumber + currentDepositNumber;
+    // document.getElementById('current-deposit').innerText = totalDeposit;
 
-    document.getElementById('current-deposit').innerText = totalDeposit;
+    updateSpanText('current-deposit', depositNumber);
+    updateSpanText('current-balance', depositNumber);
+
     document.getElementById('deposit-input').value = '';
 })
+
+//withdraw button event handler
+const withdrawBtn = document.getElementById('add-withdraw');
+withdrawBtn.addEventListener('click', function () {
+   const withdrawNumber = getInputNumber('withdraw-amount')
+
+   updateSpanText('current-withdraw', withdrawNumber);
+   updateSpanText('current-balance', -1 * withdrawNumber);
+
+   document.getElementById('withdraw-amount').value = '';
+})
+function getInputNumber(id) {
+    const amount = document.getElementById(id).value;
+    const amountNumber = parseFloat(amount);
+    return amountNumber;
+}
+
+function updateSpanText(id, addedNumber) {
+    const current = document.getElementById(id).innerText;
+    const currentNumber = parseFloat(current);
+    const totalAmount = addedNumber + currentNumber;
+    document.getElementById(id).innerText = totalAmount;
+}
